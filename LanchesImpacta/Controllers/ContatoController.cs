c450275ee75873a1e7df13330ef6681d5c996783
Fabio <fabiomtv@outlook.com.br>
2024-09-13 17:53:34 -0300

    Criando view contato e finalizando index

---
+++ LanchesImpacta/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesImpacta.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
