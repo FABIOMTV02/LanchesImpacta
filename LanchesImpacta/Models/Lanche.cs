c1aeb50fcd551d4d00a778f320977d138bdbef24
Fabio <fabiomtv@outlook.com.br>
2024-09-13 14:28:16 -0300

    Criando ModelLanches

---
+++ LanchesImpacta/Models/Lanche.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
+{
+    [Table("Lanches")]
+    public class Lanche
+    {
+        [Key]
+        public int LancheId { get; set; }
+
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Display(Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do Lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
+        public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "O descrição detalhada do lanche deve ser informada")]
+        [Display(Name = "Descrição detalhada do Lanche")]
+        [MinLength(20, ErrorMessage = "Descrição detalhada deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição detalhada pode exceder {1} caracteres")]
+        public string DescricaoDetalhada { get; set; }
+
+    }
+}
