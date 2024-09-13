f60075c5ba85276628fcdb3079233fbd075b8dc7
Fabio <fabiomtv@outlook.com.br>
2024-09-13 17:33:38 -0300

    Criando carrinho compra

---
+++ LanchesImpacta/ViewModels/CarrinhoCompraViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LanchesImpacta.Models;
+
+namespace LanchesImpacta.ViewModels
+{
+    public class CarrinhoCompraViewModel
+    {
+        public CarrinhoCompra CarrinhoCompra { get; set; }
+        public decimal CarrinhoCompraTotal { get; set; }
+    }
+}
